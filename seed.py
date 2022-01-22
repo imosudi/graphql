f277da7d9f8846f1997b0bb5320bbf02fa582304
Mosudi I. O <imosudi@gmail.com>
2022-01-22 10:36:01 +0100

    Initial app configuration

--- seed.py
+++ seed.py
@@ -3,8 +3,12 @@ from app import db, User, Post
 db.create_all()     # create tables from models
 
 user1 = User(
-    name="Sadhan Sarker",
-    email='cse.sadhan@gmail.com'
+    name="Mosudi Isiaka",
+    email='imosudi@gmail.com'
+)
+user2 = User(
+    name="Mosud Olukayode",
+    email='imosudi@yahoo.com'
 )
 
 post1 = Post()
@@ -12,8 +16,21 @@ post1.title = "Blog Post Title 1"
 post1.body = "This is the first blog post 1"
 post1.author = user1
 
+post2 = Post()
+post2.title = "Blog Post Title 2"
+post2.body = "This is the 2nd blog post 2"
+post2.author = user2
+
+post3 = Post()
+post3.title = "Blog Post Title 3"
+post3.body = "This is yet another blog post 3"
+post3.author = user1
+
 db.session.add(post1)
 db.session.add(user1)
+db.session.add(user2)
+db.session.add(post2)
+db.session.add(post3)
 db.session.commit()
 
 print(User.query.all())
